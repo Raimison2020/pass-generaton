07841d35631b74a731f557f0fa54e35f73e6b2e5
Raimison <raimison.ti@gmail.com>
2025-04-22 20:37:34 -0400

    Função generatePassword

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ const charaters = {
     symbols: "!$%&|[](){}:;.,*+-#@<>~"
 }
 
+const generatePassword = () => {
+    let staticPassword = "",
+        randomPassword = "",
+        excludeDuplicate = false,
+        passLength = lengthSlider.value;
+
+    options.forEach(option => {
+        if (option.checked) {
+            if (option.id !== "exc-duplicate" && option.id !== "spaces") {
+                staticPassword += characteres[option.id]
+            } else if (option.id === "spaces") {
+                staticPassword += `  ${staticPassword}  `
+            } else {
+                excludeDuplicate = true
+            }
+        }
+    });
+
+    for (let i = 0; i < passLength; i++) {
+        let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
+        if (excludeDuplicate) {
+            !randomPassword.includes(randomChar) | randomChar == " " ?
+            randomPassword += randomChar : i--
+        } else {
+            randomPassword += randomChar
+        }
+        passwordInput.value = randomPassword
+    }
+
+}
