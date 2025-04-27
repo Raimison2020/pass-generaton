82df45cb3232ec45ed0e92262081e55b670e2fac
Raimison <raimison.ti@gmail.com>
2025-04-27 12:19:57 -0400

    Funções updateSlider, copyPassword e criação dos addEventListener

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const passwordInput = document.querySelector(".input-box input");
 const passIndicator = document.querySelector(".pass-indicator");
 const generateBtn = document.querySelector(".generate-btn");
 
-const charaters = {
+const characters = {
     lowercase: "abcdefghijlmnoprqrstuvwxyz",
     uppercase: "ABCDEFGHIJLMNOPRSTUVWXYZ",
     numbers: "0123456789",
@@ -21,7 +21,7 @@ const generatePassword = () => {
     options.forEach(option => {
         if (option.checked) {
             if (option.id !== "exc-duplicate" && option.id !== "spaces") {
-                staticPassword += characteres[option.id]
+                staticPassword += characters[option.id]
             } else if (option.id === "spaces") {
                 staticPassword += `  ${staticPassword}  `
             } else {
@@ -34,7 +34,7 @@ const generatePassword = () => {
         let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
         if (excludeDuplicate) {
             !randomPassword.includes(randomChar) | randomChar == " " ?
-            randomPassword += randomChar : i--
+                randomPassword += randomChar : i--
         } else {
             randomPassword += randomChar
         }
@@ -46,3 +46,26 @@ const generatePassword = () => {
 const updatePassIndicator = () => {
     passIndicator.id = lengthSlider.value <= 8 ? "weak" : lengthSlider.value <= 16 ? "medium" : "strong"
 }
+
+const updateSlider = () => {
+    document.querySelector(".pass-length span").innerText = lengthSlider.value
+    generatePassword()
+    updatePassIndicator()
+}
+
+updateSlider()
+
+const copyPassword = () => {
+    navigator.clipboard.writeText(passwordInput.value)
+    copyIcon.innerText = "check"
+    copyIcon.style.color = "#4285f4"
+
+    setTimeout(() => {
+        copyIcon.innerText = "copy_all"
+        copyIcon.style.color = "#707070"
+    }, 1500)
+}
+
+copyIcon.addEventListener("click", copyPassword)
+lengthSlider.addEventListener("input", updateSlider)
+generateBtn.addEventListener("click", generatePassword)
